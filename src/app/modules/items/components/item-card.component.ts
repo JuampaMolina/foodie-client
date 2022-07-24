1611964cb7fde0e3cbe3d558a0f3589aa848bdfa
JuampaMolina <jpruizmolina@gmail.com>
2022-07-24 04:02:00 +0200

    mostrar categoría en item-card

--- src/app/modules/items/components/item-card.component.ts
+++ src/app/modules/items/components/item-card.component.ts
@@ -7,9 +7,11 @@ import { Item } from '../interface/item';
    <div
     *ngIf="item"
     class="bg-slate-200 flex flex-col text-slate-800 rounded-md p-4 max-w-sm h-32">
-    <span class="font-semibold">{{item.name}}</span>
+    <div class="space-x-2">
+      <span class="font-semibold">{{item.name}}</span>
+      <span class="font-extralight text-xs">{{item.category}}</span>
+    </div>
     <span class="font-light grow">{{item.description}}</span>
-    <!-- <p>{{item.category}}</p> -->
     <span class="text-right ">{{item.price}} EUR</span>
    </div>
   `,
